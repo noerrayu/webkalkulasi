3e8457a6417f0a98adc11ea30d8cb906ea69463a
Noer Ayu <ayu.intan117@gmail.com>
2020-04-19 00:06:13 +0700

    mengganti warna sesuai kondisi

--- script/main.js
+++ script/main.js
@@ -19,4 +19,24 @@ function calc() {
     if (oper === 'x') {
         document.getElementById('result').value = n1 * n2;
     }
+
+    var num = document.getElementById('result').value;
+
+    if (num == 0) {
+        document.getElementById("result").style.backgroundColor = 'blue';
+    } else if ((num % 2) !== 0) {
+        document.getElementById("result").style.backgroundColor = 'yellow';
+    } else if ((num % 1) == 0) {
+        document.getElementById("result").style.backgroundColor = 'green';
+    }
 }
+
+// var i = 0;
+// $(".result").each(function (i) {
+
+//     if (i % 2 == 0) {
+//         $(this).addClass("redBox");
+//     } else
+//         $(this).addClass("whiteBox")
+
+// });
